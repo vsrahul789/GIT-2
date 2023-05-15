1e15ddf71a5b05da5dcb0aebd161d252faf54722
VSRahul789 <sairahul.k64@gmail.com>
2023-05-15 15:59:14 +0530

    Web-Crawler added for testing 'URL' and 'Accessing URL for HTML elements

--- Java-Script/Web-Crawler/Crawler.js
+++ Java-Script/Web-Crawler/Crawler.js
@@ -1,13 +1,39 @@
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString) 
-    const hostpath = `${urlObj.hostname}${urlObj.pathname}`
+const {JSDOM} = require ('jsdom')
 
-    if(hostpath.length>0 && hostpath.slice(-1) === '/'){
-        return hostpath.slice(0,-1)
+
+
+function normalizeURL(urlString){
+    if(urlString.length < 1){
+        return urlString
+    } else{
+       const urlObj = new URL(urlString) 
+       const hostpath = `${urlObj.hostname}${urlObj.pathname}`
+
+       if(hostpath.length>0 && hostpath.slice(-1) === '/'){
+           return hostpath.slice(0,-1)
+       }
+       return hostpath
     }
-    return hostpath
+}
+
+function getURLfromHTML(htmlBody , baseURL){
+    const url = []
+    const dom = new JSDOM(htmlBody)
+    const linkElements = dom.window.document.querySelectorAll('a')
+
+    for( const linkElement of linkElements){
+        if(linkElement.href.slice(0,1) === '/'){
+            //relative
+            url.push(`${baseURL}${linkElement.href}`)
+        }else{
+            //absolute
+            url.push(linkElement.href)
+        }
+    }
+    return url
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLfromHTML
 }
--- Java-Script/Web-Crawler/Crawler.test.js
+++ Java-Script/Web-Crawler/Crawler.test.js
@@ -1,12 +1,12 @@
-const {normalizeURL} = require('./Crawler.js')
+const {normalizeURL,getURLfromHTML} = require('./Crawler.js')
 const {test , expect} = require('@jest/globals')
 
-// test('normalizeURL',() => {
-//     const input = ''
-//     const actual =normalizeURL(input)
-//     const expected = ''
-//     expect(actual).toEqual(expected)
-// })
+test('normalizeURL',() => {
+    const input = ''
+    const actual =normalizeURL(input)
+    const expected = ''
+    expect(actual).toEqual(expected)
+})
 test('normalizeURL strip protocal',() => {
     const input = 'https://blog.boot.dev/path'
     const actual =normalizeURL(input)
@@ -33,3 +33,55 @@ test('normalizeURL strip http',() => {
     expect(actual).toEqual(expected)
 })
 
+test('getURLfromHTML absolute',() => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href = "https://blog.boot.dev/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev/path/"
+    const actual = getURLfromHTML(inputHTML , inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLfromHTML Relative',() => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href = "/path/">
+                Boot.dev Blog
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLfromHTML(inputHTML , inputBaseURL)
+    const expected = ["https://blog.boot.dev/path/"]
+    expect(actual).toEqual(expected)
+})
+
+
+test('getURLfromHTML both',() => {
+    const inputHTML = `
+    <html>
+        <body>
+            <a href = "https://blog.boot.dev/path1/">
+                Boot.dev Blog path 1
+            </a>
+            <a href = "/path2/">
+                Boot.dev Blog path 2
+            </a>
+        </body>
+    </html>
+    `
+    const inputBaseURL = "https://blog.boot.dev"
+    const actual = getURLfromHTML(inputHTML , inputBaseURL)
+    const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"]
+    expect(actual).toEqual(expected)
+})
